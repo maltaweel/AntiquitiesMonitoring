66e3917d7160deadf7256781fd78dc6c64522f42
Mark Altaweel <maltaweel@ucl.ac.uk>
2020-02-18 00:04:46 +0000

    commit

--- src/analysis/sendEmail.py
+++ src/analysis/sendEmail.py
@@ -9,8 +9,20 @@ from email.mime.base import MIMEBase
 from email.mime.text import MIMEText
 from email.utils import formatdate
 from email import encoders
+import os
 
-def send_mail(send_from,send_to,subject,text,files,server,port,username='',password='',isTls=True):
+'''
+The file output path for printing results (to the src level of this project)
+@return rn- the output file path to use to the src level
+''' 
+def filenameToOutput():
+        
+        pn=os.path.abspath(__file__)
+        pn=pn.split("src")[0]  
+        
+        return pn
+
+def send_mail(send_from,send_to,subject,text,server,port,username='',password='',isTls=True):
     msg = MIMEMultipart()
     msg['From'] = send_from
     msg['To'] = send_to
@@ -18,10 +30,12 @@ def send_mail(send_from,send_to,subject,text,files,server,port,username='',passw
     msg['Subject'] = subject
     msg.attach(MIMEText(text))
 
+    filename=os.path.join(filenameToOutput(),'output','scotlandFinds.csv')
+    
     part = MIMEBase('application', "octet-stream")
-    part.set_payload(open("WorkBook3.xlsx", "rb").read())
+    part.set_payload(open(filename, "rb").read())
     encoders.encode_base64(part)
-    part.add_header('Content-Disposition', 'attachment; filename="WorkBook3.xlsx"')
+    part.add_header('Content-Disposition', 'attachment; filename="scrapedOutput.csv"')
     msg.attach(part)
 
     #context = ssl.SSLContext(ssl.PROTOCOL_SSLv3)
@@ -33,4 +47,4 @@ def send_mail(send_from,send_to,subject,text,files,server,port,username='',passw
     smtp.sendmail(send_from, send_to, msg.as_string())
     smtp.quit()
     
-send_mail('markaltaweel@gmail.com','maltaweel@yahoo.com','This is it','This is the text')
+send_mail('antiquitiies.scotland@gmail.com','maltaweel@yahoo.com','This is it','This is the text','smtp.gmail.com',587,'antiquitiies.scotland','prath4a$')
